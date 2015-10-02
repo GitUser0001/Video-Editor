532234a9ada2e7aeb3154499148481416836cfa2
Danylo <goddan333@gmail.com>
2015-10-02 23:21:55 +0300

    Change fxCop rules and StyleCop + Add stromg Name

--- Sourse/VideoEditor/VideoEditor/App.xaml.cs
+++ Sourse/VideoEditor/VideoEditor/App.xaml.cs
@@ -8,7 +8,7 @@ using System.Windows;
 
 namespace VideoEditor
 {
-    /// <summary>
+    ///<summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
---
+++ Sourse/VideoEditor/VideoEditor/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+//[assembly: AssemblyTitle("VideoEditor v1.0.0-alpha")]
+//[assembly: AssemblyDescription("ПО для составления видео-коллажей из нескольких видео-, аудио- и фото-источников.")]
+//[assembly: AssemblyConfiguration("")]
+//[assembly: AssemblyCompany("")]
+//[assembly: AssemblyProduct("VideoEditor")]
+//[assembly: AssemblyCopyright("Copyright ©  2015")]
+//[assembly: AssemblyTrademark("")]
+//[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+
+//[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+
+[assembly: Guid("1a73c043-ccfc-45aa-b215-aa531b0ed607")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+
+//[assembly: AssemblyVersion("1.0.0.1")]
+//[assembly: AssemblyFileVersion("1.0.0.1")]
