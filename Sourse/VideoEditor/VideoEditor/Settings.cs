0e2e3031c6417db413f8201b0687dca560fb4ece
Danylo <goddan333@gmail.com>
2015-10-05 18:45:32 +0300

    Add Settings (using singleton). Change StyleCop rules

---
+++ Sourse/VideoEditor/VideoEditor/Settings.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoEditor
+{
+    public class Settings
+    {
+        private static Settings _instance;
+
+        private Settings();
+
+        public static Settings GetInstance()
+        {
+            if (_instance == null)
+            {
+                lock (typeof(Settings))
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new Settings();
+                    }
+                }
+            }
+
+            return _instance;
+        }
+    }
+}
